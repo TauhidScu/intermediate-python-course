5204a8592c1e108e4aeaf5a0e321984c57d110d6
MD Tauhidul Islam <tauhidscu16@gmail.com>
2020-10-19 19:58:09 +0800

    Tuorial completed

--- dice_roller.py
+++ dice_roller.py
@@ -1,13 +1,18 @@
 import random
 
 def main():
-  dice_rolls = 2
+  dice_size = int(input('How many dice would you like to roll? '))
   die_sum =0
-  for i in range( 0, dice_rolls):
-    roll = random.randint(1,6)
+  for i in range( 0, dice_size):
+    roll = random.randint(1,dice_size)
+    if roll == 1 :
+      print(f'you rolld a {roll}! critical fail')
+    elif roll == dice_size :
+      print(f'you rolled a {roll}! critical success')
+    else:
+      print(f'you rolled a {roll}.')
     die_sum = die_sum + roll
-    print(f'You rolled a {roll}')   
-  #By putting an f right before the quotes of the string you're printing
+  
   print(f'you have rolled a total of {die_sum}')
 
 if __name__== "__main__":
